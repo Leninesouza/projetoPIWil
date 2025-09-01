ef8ecd084b292c8602b92db8dae7ff5b50eefaa6
LENINE SANTOS SOUZA <lenine.ssouza@senacsp.edu.br>
2025-09-01 20:18:23 -0300

    first-commit

---
+++ src/main/java/br/com/aulawilson/projetowilson/ProjetowilsonApplication.java
@@ -0,0 +1,13 @@
+package br.com.aulawilson.projetowilson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowilsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowilsonApplication.class, args);
+	}
+
+}
